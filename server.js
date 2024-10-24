72fe4b2783ac0dd38a980c8fdff470ad0ab58d77
Nico Martinez <nmartinezlandi@gmail.com>
2024-10-24 09:32:29 -0300

    arreglo error de conexion

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
