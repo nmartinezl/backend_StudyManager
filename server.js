8a074d51227374e5d54ff6891ccc41a438b19e9f
Nico Martinez <nmartinezlandi@gmail.com>
2024-10-24 10:40:31 -0300

    modificacion de todo el backend

--- server.js
+++ server.js
@@ -2,21 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-const bcrypt = require('bcryptjs'); // Importar bcryptjs
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Servir archivos estáticos (HTML, CSS, JS, etc.)
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(cors({
-    origin: '*',  // Permite cualquier origen
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Permite estos métodos
-    allowedHeaders: ['Content-Type']  // Permite estos encabezados
-}));
 
 // Crear un pool de conexiones
 const db = mysql.createPool({
@@ -32,17 +26,17 @@ function handleDisconnect() {
     db.getConnection((err, connection) => {
         if (err) {
             console.error('Error al conectar a la base de datos:', err);
-            setTimeout(handleDisconnect, 2000); // Intentar reconectar después de 2 segundos
+            setTimeout(handleDisconnect, 2000);
         } else {
             console.log('Conexión a la base de datos MySQL establecida.');
-            connection.release(); // Liberar la conexión
+            connection.release();
         }
     });
 
     db.on('error', (err) => {
         console.error('Error en la base de datos:', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect(); // Reconectar en caso de pérdida de conexión
+            handleDisconnect();
         } else {
             throw err;
         }
@@ -51,15 +45,9 @@ function handleDisconnect() {
 
 handleDisconnect();
 
-//Control de Errores en General:
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Algo salió mal en el servidor.');
-});
-
-// Verificar si el usuario admin ya existe
+// Verificar si el usuario admin ya existe y crearlo si no
 const adminEmail = 'admin@admin.com';
-const adminPassword = '1234';  // Contraseña por defecto
+const adminPassword = '1234';
 
 db.query('SELECT * FROM estudiantes WHERE email = ?', [adminEmail], (err, results) => {
     if (err) {
@@ -67,8 +55,7 @@ db.query('SELECT * FROM estudiantes WHERE email = ?', [adminEmail], (err, result
         return;
     }
     if (results.length === 0) {
-        // Si el usuario admin no existe, lo creamos
-        bcrypt.hash(adminPassword, 10, (err, hash) => {  // Hasheamos la contraseña
+        bcrypt.hash(adminPassword, 10, (err, hash) => {
             if (err) {
                 console.error('Error al hashear la contraseña de admin:', err);
                 return;
@@ -88,23 +75,21 @@ db.query('SELECT * FROM estudiantes WHERE email = ?', [adminEmail], (err, result
     }
 });
 
-
 // Ruta para registrar un estudiante
 app.post('/register', (req, res) => {
     const { nombre, apellido, dni, email, password } = req.body;
 
     bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
-            return res.status(500).json({ success: false, message: 'Error al hashear la contraseña: ' + err.message });
+            return res.status(500).json({ success: false, message: 'Error al hashear la contraseña' });
         }
 
         const query = 'INSERT INTO estudiantes (nombre, apellido, dni, email, password, activo) VALUES (?, ?, ?, ?, ?, 1)';
         db.query(query, [nombre, apellido, dni, email, hash], (err, result) => {
             if (err) {
-                return res.status(500).json({ success: false, message: 'Error al registrar usuario: ' + err.message });
+                return res.status(500).json({ success: false, message: 'Error al registrar usuario' });
             }
-            // Devolver más información, como el ID del usuario
-            res.status(201).json({ success: true, message: 'Usuario registrado con éxito', userId: result.insertId });
+            res.status(201).json({ success: true, message: 'Usuario registrado con éxito' });
         });
     });
 });
@@ -116,30 +101,25 @@ app.post('/login', (req, res) => {
     
     db.query(query, [email], (err, results) => {
         if (err) {
-            return res.status(500).json({ success: false, message: 'Error al iniciar sesión: ' + err.message });
+            return res.status(500).json({ success: false, message: 'Error al iniciar sesión' });
         }
         if (results.length > 0) {
             const user = results[0];
 
-            // Verificar si el usuario está activo
             if (user.activo === 0) {
-                return res.status(401).json({ success: false, message: 'Usuario desactivado. Por favor, comuníquese con el administrador.' });
+                return res.status(401).json({ success: false, message: 'Usuario desactivado' });
             }
 
-            // Comparar la contraseña ingresada con la contraseña hasheada almacenada
             bcrypt.compare(password, user.password, (err, isMatch) => {
                 if (err) {
-                    return res.status(500).json({ success: false, message: 'Error al comparar contraseñas: ' + err.message });
+                    return res.status(500).json({ success: false, message: 'Error al comparar contraseñas' });
                 }
                 if (isMatch) {
-                    const userData = {
-                        id: user.id,
-                        nombre: user.nombre,
-                        apellido: user.apellido,
-                        email: user.email,
-                        activo: user.activo
-                    };
-                    res.json({ success: true, user: userData });
+                    if (user.nombre === 'Admin') {
+                        res.json({ success: true, role: 'admin' });
+                    } else {
+                        res.json({ success: true, role: 'user' });
+                    }
                 } else {
                     res.status(401).json({ success: false, message: 'Correo o contraseña incorrectos' });
                 }
@@ -147,90 +127,6 @@ app.post('/login', (req, res) => {
         } else {
             res.status(401).json({ success: false, message: 'Correo o contraseña incorrectos' });
         }
-    });
-});
-
-
-// Ruta para obtener todas las carreras
-app.get('/carreras', (req, res) => {
-    const limit = parseInt(req.query.limit) || 10; // Límite de registros por página (default: 10)
-    const offset = parseInt(req.query.offset) || 0; // Paginación: inicio de los registros
-    const query = 'SELECT * FROM carreras LIMIT ? OFFSET ?';
-
-    db.query(query, [limit, offset], (err, results) => {
-        if (err) {
-            return res.status(500).send('Error al obtener las carreras: ' + err.message);
-        }
-        res.json(results);  // Devolver las carreras como JSON
-    });
-});
-
-// Ruta para obtener los años disponibles para una carrera
-app.get('/anios/:carreraId', (req, res) => {
-    const carreraId = req.params.carreraId;
-    const query = 'SELECT DISTINCT anio FROM materias WHERE carrera_id = ? ORDER BY anio';
-    db.query(query, [carreraId], (err, results) => {
-        if (err) {
-            return res.status(500).send('Error al obtener los años: ' + err.message);
-        }
-        const anios = results.map(row => row.anio);
-        res.json(anios);  // Devolver los años como JSON
-    });
-});
-
-// Ruta para obtener las materias de un año específico de una carrera
-app.get('/materias/:carreraId/:anio', (req, res) => {
-    const carreraId = req.params.carreraId;
-    const anio = req.params.anio;
-    const query = 'SELECT * FROM materias WHERE carrera_id = ? AND anio = ?';
-    db.query(query, [carreraId, anio], (err, results) => {
-        if (err) {
-            return res.status(500).send('Error al obtener las materias: ' + err.message);
-        }
-        res.json(results);  // Devolver las materias como JSON
-    });
-});
-
-
-
-/// Ruta para matricular a un usuario en una materia
-app.post('/matricular', (req, res) => {
-    const { usuarioId, materiaId } = req.body;  // Obtener el ID del usuario y de la materia desde el cuerpo de la solicitud
-
-    // Primero, verificar si la materia existe y obtener su nombre
-    const materiaQuery = 'SELECT nombre FROM materias WHERE id = ?';
-    db.query(materiaQuery, [materiaId], (err, materiaResults) => {
-        if (err) {
-            return res.status(500).send('Error al verificar la materia: ' + err.message);
-        }
-        if (materiaResults.length === 0) {
-            return res.status(404).send('La materia no existe');
-        }
-
-        const nombreMateria = materiaResults[0].nombre;
-
-        // Luego, insertar la inscripción
-        const query = 'INSERT INTO inscripciones (estudiante_id, materia_id, estado) VALUES (?, ?, ?)';
-        db.query(query, [usuarioId, materiaId, 'activa'], (err, result) => {  // Insertar con estado 'activa'
-            if (err) {
-                return res.status(500).send('Error al matricular al usuario: ' + err.message);
-            }
-            res.send(`Usuario matriculado con éxito en la materia: ${nombreMateria}`);
-        });
-    });
-});
-
-
-//Endpoint para que los estudiantes puedan ver en qué materias están matriculados.
-app.get('/materias-matriculadas/:usuarioId', (req, res) => {
-    const usuarioId = req.params.usuarioId;
-    const query = 'SELECT m.nombre FROM inscripciones i JOIN materias m ON i.materia_id = m.id WHERE i.estudiante_id = ? AND i.estado = "activa"';
-
-    db.query(query, [usuarioId], (err, results) => {
-        if (err) {
-            return res.status(500).send('Error al obtener las materias matriculadas: ' + err.message);
-        }
-        res.json(results);
     });
 });
 
